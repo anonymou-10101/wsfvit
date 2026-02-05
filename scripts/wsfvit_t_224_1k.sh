50bcdd86cf979e6fff487792406adcfed0d7a87f
anonymou-10101 <anonymousgithub46@gmail.com>
2026-02-05 16:26:49 +0900

    Update and rename mixvit_t_224_1k.sh to wsfvit_t_224_1k.sh

--- scripts/wsfvit_t_224_1k.sh
+++ scripts/wsfvit_t_224_1k.sh
@@ -16,7 +16,7 @@ else
 fi
 
 torchrun --nproc_per_node=${GPUS} --master_addr=127.0.0.1 --master_port=23456 train.py ${DATA_PATH} \
-         --model mixvit_t_224 --img-size 224 --epochs 300 --batch-size 128 \
+         --model wsfvit_t_224 --img-size 224 --epochs 300 --batch-size 128 \
          --opt adamw --weight-decay .05 --lr 3e-3 --sched cosine --warmup-epochs 30 \
          --warmup-lr 1e-6 --min-lr 1e-5 --drop-path .2 --smoothing 0.1 \
          --mixup .5 --cutmix 1.0 --clip-grad 1.0 \
